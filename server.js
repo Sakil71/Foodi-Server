e35e96d91fa977aa3e84481539e76c92cd7499fe
Shakil Ahamed <shakilahamed2171@gmail.com>
2024-07-12 05:01:59 +0600

    user, cart and food controller

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 import express from "express";
 import cors from "cors";
+import 'dotenv/config';
 import { connectDb } from "./config/db.js";
 import foodRouter from "./routes/foodRouter.js";
+import userRouter from "./routes/userRouter.js";
+import cartRouter from "./routes/cartRouter.js";
 
 
 // App Config
@@ -19,13 +22,15 @@ connectDb();
 // API endpoints
 app.use("/api/food", foodRouter);
 app.use("/images", express.static("uploads"));
+app.use("/api/user", userRouter);
+app.use("/api/cart", cartRouter);
 
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.send("Server Working");
 })
 
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`server starting on http://localhost:${port}`);
 })
